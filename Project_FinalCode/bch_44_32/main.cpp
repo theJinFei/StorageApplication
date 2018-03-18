49f7396d2f9a39fcd2e8e083d130ad181241d6d8
theJinFei <34594300+theJinFei@users.noreply.github.com>
2018-03-18 16:48:21 +0800

    Add files via upload

---
+++ Project_FinalCode/bch_44_32/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "BCHCode.h"
+//#include <sys/time.h>
+using namespace std;
+
+
+unsigned long Total_time = 0;
+int main()
+{
+	//struct  timeval  start_time;
+	//struct  timeval  end_time;
+	//gettimeofday(&start_time, NULL);
+	BCHCode *bchCode = new BCHCode();
+	bchCode->testBCH();
+	//gettimeofday(&end_time, NULL);
+	//Total_time += 1000000 * (end_time.tv_sec-start_time.tv_sec)+ end_time.tv_usec-start_time.tv_usec;
+	//cout << "\tTotal operation Time is " << (double)Total_time  / 1000000 << " s" << endl;
+	//bchCode -> errTest();
+	return 0;
+}
+
+
